b40b29e806ccf31654a8468bc4fe6757a737036d
Stuart Kwan <skwan@microsoft.com>
2015-11-04 18:28:39 -0800

    Force https

--- TDLR/Global.asax.cs
+++ TDLR/Global.asax.cs
@@ -19,5 +19,12 @@ namespace Tdlr
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        protected void Application_BeginRequest(Object sender, EventArgs e)
+        {
+            if (HttpContext.Current.Request.IsSecureConnection.Equals(false))
+            {
+                Response.Redirect("https://" + Request.ServerVariables["HTTP_HOST"] + HttpContext.Current.Request.RawUrl);
+            }
+        }
     }
 }
